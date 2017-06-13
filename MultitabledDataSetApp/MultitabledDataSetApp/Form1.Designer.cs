14bb80b0581ee7081d9eba3292897cb35bac1838
NuCode1497 <neuburgercody@gmail.com>
2017-06-13 14:55:39 -0700

    Tutorial 9. MultitabledDataSetApp

---
+++ MultitabledDataSetApp/MultitabledDataSetApp/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace MultitabledDataSetApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.InventoryDataGridView = new System.Windows.Forms.DataGridView();
+            this.CustomersDataGridView = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.OrdersDataGridView = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.GetOrderTextBox = new System.Windows.Forms.TextBox();
+            this.GetOrdersButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.InventoryDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersDataGridView)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current Inventory";
+            // 
+            // InventoryDataGridView
+            // 
+            this.InventoryDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InventoryDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.InventoryDataGridView.Location = new System.Drawing.Point(13, 30);
+            this.InventoryDataGridView.Name = "InventoryDataGridView";
+            this.InventoryDataGridView.Size = new System.Drawing.Size(545, 147);
+            this.InventoryDataGridView.TabIndex = 1;
+            // 
+            // CustomersDataGridView
+            // 
+            this.CustomersDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CustomersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomersDataGridView.Location = new System.Drawing.Point(13, 197);
+            this.CustomersDataGridView.Name = "CustomersDataGridView";
+            this.CustomersDataGridView.Size = new System.Drawing.Size(545, 147);
+            this.CustomersDataGridView.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Current Customers";
+            // 
+            // OrdersDataGridView
+            // 
+            this.OrdersDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OrdersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OrdersDataGridView.Location = new System.Drawing.Point(13, 364);
+            this.OrdersDataGridView.Name = "OrdersDataGridView";
+            this.OrdersDataGridView.Size = new System.Drawing.Size(545, 147);
+            this.OrdersDataGridView.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Current Orders";
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.UpdateButton.Location = new System.Drawing.Point(483, 517);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+            this.UpdateButton.TabIndex = 6;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.GetOrdersButton);
+            this.groupBox1.Controls.Add(this.GetOrderTextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(13, 518);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 100);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lookup Customer Order";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Customer ID:";
+            // 
+            // GetOrderTextBox
+            // 
+            this.GetOrderTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GetOrderTextBox.Location = new System.Drawing.Point(82, 31);
+            this.GetOrderTextBox.Name = "GetOrderTextBox";
+            this.GetOrderTextBox.Size = new System.Drawing.Size(133, 20);
+            this.GetOrderTextBox.TabIndex = 1;
+            // 
+            // GetOrdersButton
+            // 
+            this.GetOrdersButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.GetOrdersButton.Location = new System.Drawing.Point(110, 71);
+            this.GetOrdersButton.Name = "GetOrdersButton";
+            this.GetOrdersButton.Size = new System.Drawing.Size(105, 23);
+            this.GetOrdersButton.TabIndex = 2;
+            this.GetOrdersButton.Text = "Get Order Details";
+            this.GetOrdersButton.UseVisualStyleBackColor = true;
+            this.GetOrdersButton.Click += new System.EventHandler(this.GetOrdersButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 630);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.OrdersDataGridView);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CustomersDataGridView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InventoryDataGridView);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "AutoLot Database Manipulator";
+            ((System.ComponentModel.ISupportInitialize)(this.InventoryDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersDataGridView)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView InventoryDataGridView;
+        private System.Windows.Forms.DataGridView CustomersDataGridView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView OrdersDataGridView;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button GetOrdersButton;
+        private System.Windows.Forms.TextBox GetOrderTextBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MultitabledDataSetApp/MultitabledDataSetApp/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace MultitabledDataSetApp
+{
+    public partial class Form1 : Form
+    {
+        //form wide dataset
+        private DataSet _autoLotDs = new DataSet("AutoLot");
+
+        //make use of command builders to simplify data adapter configuration
+        private SqlCommandBuilder _sqlCbInventory;
+        private SqlCommandBuilder _sqlCbCustomers;
+        private SqlCommandBuilder _sqlCbOrders;
+
+        private SqlDataAdapter _invTableAdapter;
+        private SqlDataAdapter _custTableAdapter;
+        private SqlDataAdapter _ordersTableAdapter;
+
+        private string _connectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _connectionString = ConfigurationManager.ConnectionStrings["AutoLotSqlProvider"].ConnectionString;
+
+            _invTableAdapter = new SqlDataAdapter("select * from inventory", _connectionString);
+            _custTableAdapter = new SqlDataAdapter("select * from customers", _connectionString);
+            _ordersTableAdapter = new SqlDataAdapter("select * from orders", _connectionString);
+
+            _sqlCbCustomers = new SqlCommandBuilder(_invTableAdapter);
+            _sqlCbInventory = new SqlCommandBuilder(_custTableAdapter);
+            _sqlCbOrders = new SqlCommandBuilder(_ordersTableAdapter);
+
+            _invTableAdapter.Fill(_autoLotDs, "Inventory");
+            _custTableAdapter.Fill(_autoLotDs, "Customers");
+            _ordersTableAdapter.Fill(_autoLotDs, "Orders");
+
+            BuildTableRelationship();
+
+            InventoryDataGridView.DataSource = _autoLotDs.Tables["Inventory"];
+            CustomersDataGridView.DataSource = _autoLotDs.Tables["Customers"];
+            OrdersDataGridView.DataSource = _autoLotDs.Tables["Orders"];
+        }
+
+        private void BuildTableRelationship()
+        {
+            //Create CustomerOrder data relation object
+            DataRelation dr = new DataRelation("CustomerOrder", _autoLotDs.Tables["Customers"].Columns["CustID"], _autoLotDs.Tables["Orders"].Columns["CustID"]);
+            _autoLotDs.Relations.Add(dr);
+            //Create InventoryOrder data relation object
+            dr = new DataRelation("InventoryOrder", _autoLotDs.Tables["Inventory"].Columns["CarID"], _autoLotDs.Tables["Orders"].Columns["CarID"]);
+            _autoLotDs.Relations.Add(dr);
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _invTableAdapter.Update(_autoLotDs, "Inventory");
+                _custTableAdapter.Update(_autoLotDs, "Customers");
+                _ordersTableAdapter.Update(_autoLotDs, "Orders");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void GetOrdersButton_Click(object sender, EventArgs e)
+        {
+            string strOrderInfo = string.Empty;
+
+            int custID = int.Parse(GetOrderTextBox.Text);
+
+            //based on id, get the correct row in customers table
+            var drsCust = _autoLotDs.Tables["Customers"].Select($"CustID = {custID}");
+            strOrderInfo += $"Customer {drsCust[0]["CustID"]}: {drsCust[0]["FirstName"].ToString().Trim()} {drsCust[0]["LastName"].ToString().Trim()}\n";
+
+            //Navigate from customer table to order table
+            var drsOrder = drsCust[0].GetChildRows(_autoLotDs.Relations["CustomerOrder"]);
+
+            //loop through all orders for this customer
+            foreach(DataRow order in drsOrder)
+            {
+                strOrderInfo += $"----\nOrder Number: {order["OrderID"]}\n";
+
+                //Get the car referenced by this order
+                DataRow[] drsInv = order.GetParentRows(_autoLotDs.Relations["InventoryOrder"]);
+
+                //Get infor for (SINGLE) car info for this order
+                DataRow car = drsInv[0];
+                strOrderInfo += $"Make: {car["Make"]}\n";
+                strOrderInfo += $"Color: {car["Color"]}\n";
+                strOrderInfo += $"PetName: {car["PetName"]}\n";
+            }
+
+            MessageBox.Show(strOrderInfo, "Order Details");
+        }
+    }
+}
